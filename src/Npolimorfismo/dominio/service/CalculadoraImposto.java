fd02707d502aa85a9f4ea736ef8070fb33eaaf02
Thiago <thiagofr4nc4@gmail.com>
2025-04-07 19:07:38 -0300

    a

--- src/Npolimorfismo/dominio/service/CalculadoraImposto.java
+++ src/Npolimorfismo/dominio/service/CalculadoraImposto.java
@@ -9,7 +9,7 @@ public  class CalculadoraImposto {
         double imposto = computador.calcularImposto();
         System.out.println("Computador: " + computador.getNome());
         System.out.println("Valor do computador: " + computador.getValor());
-        System.out.println("Imposto a ser pago:  " + imposto);
+        System.out.println("Imposto a ser pagoooo:  " + imposto);
     }
 
         public static void calcularImpostoTomate(Tomate tomate){
